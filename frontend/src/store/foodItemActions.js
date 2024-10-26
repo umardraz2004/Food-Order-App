aa6a3bb2a6f9d13377583f036897731167d55c64
umardraz2004 <umardraz6965@gmail.com>
2024-10-27 01:46:38 +0500

    loading setting

--- frontend/src/store/foodItemActions.js
+++ frontend/src/store/foodItemActions.js
@@ -1,7 +1,7 @@
 import { cartActions } from "./foodSlice";
 
 export const fetchFoodData = () => {
-  const mealsUrl = 'https://translucent-secret-drink.glitch.me/meals';
+  const mealsUrl = "https://translucent-secret-drink.glitch.me/meals";
   return async (dispatch) => {
     const fetchMeals = async () => {
       const response = await fetch(mealsUrl);
@@ -15,10 +15,8 @@ export const fetchFoodData = () => {
     try {
       dispatch(cartActions.setLoading(true));
       const myData = await fetchMeals();
-      setTimeout(() => {
-        dispatch(cartActions.setMeal(myData));
-        dispatch(cartActions.setLoading(false));
-      }, 3000)
+      dispatch(cartActions.setLoading(false));
+      dispatch(cartActions.setMeal(myData));
     } catch (err) {
       console.error("Error fetching data:", err);
     }
@@ -28,18 +26,21 @@ export const fetchFoodData = () => {
 export const storeData = (cartItems, customerData) => {
   return async (dispatch) => {
     try {
-      const response = await fetch('https://translucent-secret-drink.glitch.me/orders', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({
-          order: {
-            items: cartItems,
-            customer: customerData
-          }
-        })
-      });
+      const response = await fetch(
+        "https://translucent-secret-drink.glitch.me/orders",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            order: {
+              items: cartItems,
+              customer: customerData,
+            },
+          }),
+        }
+      );
       if (!response.ok) {
         throw new Error(`HTTP error! status: ${response.status}`);
       }
