6d7b73f6f644c5535fa461adf415af11810902f2
umardraz2004 <umardraz6965@gmail.com>
2024-10-27 01:56:10 +0500

    some loading screen adjustments

--- frontend/src/components/Meals.jsx
+++ frontend/src/components/Meals.jsx
@@ -4,6 +4,7 @@ import { fetchFoodData } from "../store/foodItemActions";
 import { formatter } from "../utils/Formatter";
 import { cartActions } from "../store/foodSlice";
 import { Toaster, toast } from "react-hot-toast";
+import classes from './Loading.module.css'
 
 const Meals = () => {
   const foodItems = useSelector((state) => state.meals.items);
@@ -33,33 +34,39 @@ const Meals = () => {
   console.log(foodItems);
   return (
     <>
-      {loading ? <p className="loading">
-        <div className="spinner"></div>
-      </p> : <ol id="meals">
-        {foodItems.map((item) => (
-          <li key={item.id} className="meal-item">
-            <article>
-              <img
-                src={`https://translucent-secret-drink.glitch.me/${item.image}`}
-                alt="Error loading image"
-              />
-              <h3>{item.name}</h3>
-              <div>
-                <div className="meal-item-price">
-                  {formatter.format(item.price)}
+      {loading ? (
+        <p className={classes.loading}>
+          <div className={classes.spinner}></div>
+        </p>
+      ) : (
+        <ol id="meals">
+          {foodItems.map((item) => (
+            <li key={item.id} className="meal-item">
+              <article>
+                <img
+                  src={`https://translucent-secret-drink.glitch.me/${item.image}`}
+                  alt="Error loading image"
+                />
+                <h3>{item.name}</h3>
+                <div>
+                  <div className="meal-item-price">
+                    {formatter.format(item.price)}
+                  </div>
+                  <div className="meal-item-description">
+                    {item.description}
+                  </div>
+                  <button
+                    className="button meal-item-actions"
+                    onClick={() => addToCartHandler(item)}
+                  >
+                    Add to cart
+                  </button>
                 </div>
-                <div className="meal-item-description">{item.description}</div>
-                <button
-                  className="button meal-item-actions"
-                  onClick={() => addToCartHandler(item)}
-                >
-                  Add to cart
-                </button>
-              </div>
-            </article>
-          </li>
-        ))}
-      </ol>}
+              </article>
+            </li>
+          ))}
+        </ol>
+      )}
       <Toaster />
     </>
   );
